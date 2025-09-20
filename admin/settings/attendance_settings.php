5106738b274f5a6bccd42ee0e0f9ca5a5438f0b9
Batighor Computers <ourbatighor@gmail.com>
2025-09-20 23:45:14 +0600

    Update attendance_settings.php

--- admin/settings/attendance_settings.php
+++ admin/settings/attendance_settings.php
@@ -91,5 +91,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
     <?php include '../inc/footer.php'; ?>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
 </body>
 </html>
