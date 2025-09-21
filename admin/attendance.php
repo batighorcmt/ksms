ad6fc5dfea68cdc0414c6d379c8a8dad4fa50def
Batighor Computers <ourbatighor@gmail.com>
2025-09-21 23:55:29 +0600

    Update attendance.php

--- admin/attendance.php
+++ admin/attendance.php
@@ -232,7 +232,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['mark_attendance'])) {
             $pdo->rollBack();
             $_SESSION['error'] = "উপস্থিতি রেকর্ড করতে সমস্যা হয়েছে: " . $e->getMessage();
         }
-
+// Check if SMS should be sent for this status
+if (should_send_attendance_sms($status)) {
+    // Get student mobile number from database
+    $student_mobile = $student_map[$student_id]['mobile_number'] ?? '';
+    
+    if (!empty($student_mobile)) {
+        // Get SMS template for this status
+        $template = get_sms_template($status);
+        
+        if ($template) {
+            // Prepare data for template
+            $template_data = [
+                'student_name' => $student_map[$student_id]['first_name'] . ' ' . $student_map[$student_id]['last_name'],
+                'roll' => $student_map[$student_id]['roll_number'],
+                'date' => $date,
+                'status' => $status,
+                'class' => $class_name,
+                'section' => $section_name
+            ];
+            
+            // Process template
+            $message = process_sms_template($template, $template_data);
+            
+            // Send SMS
+            $sms_sent = send_sms($student_mobile, $message);
+            
+            if ($sms_sent) {
+                // Log successful SMS sending
+                error_log("SMS sent to $student_mobile for $status status");
+            } else {
+                error_log("Failed to send SMS to $student_mobile");
+            }
+        }
+    }
+}
         // Set selected values for form
         $selected_class = $class_id;
         $selected_section = $section_id;
