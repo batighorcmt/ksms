69209074275496d5feddde7dd491c5be980e40c4
Batighor Computers <ourbatighor@gmail.com>
2025-09-21 23:52:49 +0600

    Update sms_api.php

--- admin/inc/sms_api.php
+++ admin/inc/sms_api.php
@@ -1,22 +1,40 @@
 <?php
-// inc/sms_api.php
-function send_sms($number, $message) {
-    $api_key = 'W6uYRXsPj2nLHfPJ3YCC';
-    $senderid = '8809617625226';
-    $url = "http://bulksmsbd.net/api/smsapi?api_key=$api_key&type=text&number=$number&senderid=$senderid&message=" . urlencode($message);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $response = curl_exec($ch);
-    curl_close($ch);
+// In your attendance.php file, after marking attendance:
 
-    // Check if SMS was sent successfully
-    if (strpos($response, 'SMS Submitted') !== false) {
-        return true;
-    } else {
-        error_log("SMS sending failed: " . $response);
-        return false;
+// Check if SMS should be sent for this status
+if (should_send_attendance_sms($status)) {
+    // Get student mobile number from database
+    $student_mobile = $student_map[$student_id]['mobile_number'] ?? '';
+    
+    if (!empty($student_mobile)) {
+        // Get SMS template for this status
+        $template = get_sms_template($status);
+        
+        if ($template) {
+            // Prepare data for template
+            $template_data = [
+                'student_name' => $student_map[$student_id]['first_name'] . ' ' . $student_map[$student_id]['last_name'],
+                'roll' => $student_map[$student_id]['roll_number'],
+                'date' => $date,
+                'status' => $status,
+                'class' => $class_name,
+                'section' => $section_name
+            ];
+            
+            // Process template
+            $message = process_sms_template($template, $template_data);
+            
+            // Send SMS
+            $sms_sent = send_sms($student_mobile, $message);
+            
+            if ($sms_sent) {
+                // Log successful SMS sending
+                error_log("SMS sent to $student_mobile for $status status");
+            } else {
+                error_log("Failed to send SMS to $student_mobile");
+            }
+        }
     }
 }
 ?>
