3d7c946b1ff32a2d9f79777edf3cc0cf9d4ee258
Batighor Computers <ourbatighor@gmail.com>
2025-10-16 21:22:57 +0600

    exam delete

--- exam/delete_exam.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config.php';
-if (!isAuthenticated() || !hasRole(['super_admin'])) redirect('../login.php');
-$id = intval($_GET['id'] ?? 0);
-if($id){
-  $pdo->prepare("DELETE FROM exams WHERE id=?")->execute([$id]);
-}
-header("Location: exam_list.php");
-exit;
